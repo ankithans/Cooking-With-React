a411ec62c2229ebb2eca4d7653c0269262a7155f
Ankit Hans <ankithans1947@gmail.com>
2020-12-11 17:07:53 +0530

    Updates

--- static/js/main.4d2771bd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-webdevsimplified"]=this["webpackJsonpreact-webdevsimplified"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n(1),s=n.n(r),i=n(3);function j(){return Object(c.jsx)(c.Fragment,{})}function b(){return Object(c.jsxs)(c.Fragment,{children:[Object(c.jsx)(j,{}),Object(c.jsx)(j,{})]})}var a=function(){return Object(c.jsx)(b,{})};n.n(i).a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.4d2771bd.chunk.js.map
